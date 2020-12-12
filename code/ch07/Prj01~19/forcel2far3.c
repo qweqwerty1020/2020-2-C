572a9cce33adc11019eb305c421397c94b816642
qweqwerty1020 <71211587+qweqwerty1020@users.noreply.github.com>
2020-12-12 18:40:57 +0900

    Update forcel2far3.c

--- code/ch07/Prj01~19/forcel2far3.c
+++ code/ch07/Prj01~19/forcel2far3.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	double celcius = 12.46;
 
-	printf("   ¼·¾¾(C)   È­¾¾(F)\n");
+	printf("   ì„­ì”¨(C)   í™”ì”¨(F)\n");
 	printf("-----------------------\n");
 
 	for (int i = 1; i <= MAX; i++, celcius += INCREMENT)
