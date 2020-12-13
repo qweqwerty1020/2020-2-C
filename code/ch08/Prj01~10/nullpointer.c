46ae901192c890592bd1dc5b0c19bd23974d8c1e
qweqwerty1020 <kyle1020@naver.com>
2020-12-13 12:33:27 +0900

    수정 upload

---
+++ code/ch08/Prj01~10/nullpointer.c
@@ -0,0 +1,14 @@
+﻿// file: nullpointer.c
+#include <stdio.h>
+
+int main(void)
+{
+	int *ptr1, *ptr2, data = 10;
+	ptr1 = NULL;
+
+	printf("%p\n", ptr1);
+	//printf("%p\n", ptr2); 초기값이 없어 컴파일 오류발생.
+	printf("%d\n", data);
+
+	return 0;
+}
